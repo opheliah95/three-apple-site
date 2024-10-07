c1ad9b1624be39f74d0411324c6ef3118e7c46fe
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-10-07 20:28:04 +0100

    added hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import gsap from 'gsap';
+import {useGSAP} from '@gsap/react';
+import { heroVideo } from '../utils';
+gsap.registerPlugin(useGSAP)
 const Hero = () => {
+    useGSAP(()=> {
+        gsap.to(
+            "#hero", {
+                opacity: 1,
+                transition: 2
+            }
+        )
+    }, [])
   return (
-    <div>
-      Hero
-    </div>
+    <section className="nav-height w-full bg-black relative">
+        <div className="h-5/6 w-full flex-center flex-col">
+            <p className="hero-title" id="hero">iPhone 15 Pro</p>
+        </div>
+        <div className="md:w-10/12 w-9/12">
+            <video src={heroVideo}></video>
+        </div>
+    </section>
   )
 }
 
