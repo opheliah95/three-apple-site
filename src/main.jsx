d28c71fdb95e7645a699cd599bda422a66a7ca23
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-10-08 22:14:59 +0100

    initial sentry setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,25 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://a3eec00b1e0d6c5b0ac3e1fd857d12ab@o4508088940888064.ingest.de.sentry.io/4508088948752464",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
